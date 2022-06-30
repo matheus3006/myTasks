3180ad5cd30c963330fcbf83bcd99b733201471c
matheus3006 <matheussouza18@live.com>
2022-06-30 08:02:02 -0300

    feat: add textInput & input style

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,22 +1,41 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, TextInput, StyleSheet, SafeAreaView} from 'react-native';
 
 export const Home = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Ola dev</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Welcome Dev</Text>
+
+        <TextInput style={styles.input} />
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#121214',
+  },
+
   container: {
     flex: 1,
     backgroundColor: '#121214',
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 50,
   },
   title: {
     color: '#f1f1f1',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#29292e',
+    color: '#f1f1f1',
+    fontSize: 18,
+    padding: 15,
+    marginTop: 30,
+    borderRadius: 7,
   },
 });
