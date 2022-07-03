157a966e095edca01c77bdf6e8ff7421bd8049eb
matheus3006 <matheussouza18@live.com>
2022-07-03 20:58:54 -0300

    feat: add async-storage to app, using useState to save data and useEffect to update the state as new taks are providaded, reducing the amount of times the app has to go through the database

--- src/Context/TaskContext.tsx
+++ src/Context/TaskContext.tsx
@@ -1,8 +1,11 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import React from 'react';
 
 interface IProps {
   children: React.ReactElement;
 }
+
+const taskData = '@MyTasks:Tasks';
 
 export interface ITask {
   id: string;
@@ -19,11 +22,30 @@ export const TaskContext = React.createContext<ITaskContext>(
 );
 
 export const TaskProvider: React.FunctionComponent<IProps> = ({children}) => {
-  const addTask = (task: ITask) => {};
+  const [data, setData] = React.useState<ITask[]>([]);
 
-  const tasks = [{id: '1', title: 'Task01'}];
+  React.useEffect(() => {
+    async function loadTasks() {
+      const taskList = await AsyncStorage.getItem(taskData);
+      if (taskList) {
+        setData(JSON.parse(taskList));
+      }
+    }
+    loadTasks();
+  }, []);
+
+  const addTask = async (task: ITask) => {
+    try {
+      const newTaskList = [...data, task];
+      setData(newTaskList);
+      await AsyncStorage.setItem(taskData, JSON.stringify(newTaskList));
+    } catch (err) {
+      throw new Error(err as string);
+    }
+  };
+
   return (
-    <TaskContext.Provider value={{tasks, addTask}}>
+    <TaskContext.Provider value={{tasks: data, addTask}}>
       {children}
     </TaskContext.Provider>
   );
