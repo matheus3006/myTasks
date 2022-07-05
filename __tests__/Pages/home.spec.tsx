a98f1df80b0ec09002718e01f5cc4e44a444ddb7
matheus3006 <matheussouza18@live.com>
2022-07-05 07:51:06 -0300

    test: add useTaskList test to check addTask function on custon hook

--- __tests__/Pages/home.spec.tsx
+++ __tests__/Pages/home.spec.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import {render} from '@testing-library/react-native';
+import {renderHook, act} from '@testing-library/react-hooks';
+
+import {TaskProvider, useTaskList} from '../../src/Context/TaskContext';
 import {Home} from '../../src/Pages/Home';
 
 describe('Home page', () => {
@@ -11,5 +14,16 @@ describe('Home page', () => {
     expect(inputNewText).toBeDefined();
 
     expect(inputNewText.props.placeholder).toBeTruthy();
+  });
+
+  test('Verifica a insercao de um item na list de tarefa', async () => {
+    const {result} = renderHook(() => useTaskList(), {
+      wrapper: TaskProvider,
+    });
+
+    const data = {id: 'Task01', title: 'Task01'};
+
+    await act(() => result.current.addTask(data));
+    expect(result.current.tasks).toBeTruthy();
   });
 });
