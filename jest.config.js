9dbb2490b125a9bf73cb646a70ec410b4611734e
matheus3006 <matheussouza18@live.com>
2022-07-05 11:21:31 -0300

    chore: jest.config create

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "preset": "react-native",
+  "setupFilesAfterEnv": [
+    "@testing-library/jest-native/extend-expect"
+  ],
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ]
+}
